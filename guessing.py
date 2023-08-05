1fdc9f2ef91ef5a4efeecdc92ac60d18d8709516
Nagesh Avhad <menageshavhad@gmail.com>
2023-08-05 20:43:45 +0530

    adding celender

---
+++ guessing.py
@@ -0,0 +1,15 @@
+import random
+mine = random.randrange(2, 30, 2)
+
+print("Pick one number from 1 to 20")
+ans = input("Click any for next")
+print("Double the number")
+ans = input("Click any for next")
+print("Add mine: ",mine)
+ans = input("Click any for next")
+print("divide number by 2")
+ans = input("Click any for next")
+print("Subtract your number from total")
+ans = input("Click any for next")
+ans = mine/2
+print("Answer is ",ans)
